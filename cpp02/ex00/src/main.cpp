e544b0b3b6c9399877afb97fef24ab692420ca94
Mariia Osokina <maria.v.osokina.com>
2025-08-17 14:47:03 +0100

    cpp02 ex00 is done

---
+++ cpp02/ex00/src/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/17 09:56:49 by mosokina          #+#    #+#             */
+/*   Updated: 2025/08/17 14:44:48 by mosokina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Fixed.hpp"
+#include <iostream>
+
+int main( void )
+{
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
