931eb5da3ebb01899b1be6bd9c1d19739fde25eb
Mariia Osokina <maria.v.osokina@gmail.com>
2025-08-26 19:22:13 +0100

    ex03 is done

--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -6,28 +6,41 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 09:56:49 by mosokina          #+#    #+#             */
-/*   Updated: 2025/08/22 18:22:20 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/08/26 19:19:47 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Fixed.hpp"
+#include "../include/Point.hpp"
 
-
-
-
-
-
-int main(void)
+void	testPoint( Point const a, Point const b, Point const c, Point const testPoint)
 {
-	Fixed	a;
-	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	return 0;
+	std::cout << "Test: Point with coordinates (" << testPoint.getX() << "," << testPoint.getY() <<")" << std::endl;
+	if (bsp(a, b, c, testPoint))
+		std::cout << "point is inside the triangle." << std::endl;
+	else
+		std::cout << "point is out of the triangle." << std::endl;
 }
 
+int	main(void)
+{
+	Point const a(0.0f , 0.0f);
+	Point const b(5.0f , 0.0f);
+	Point const c(0.0f , 5.0f);
+
+	Point const insidePoint1(1.0f, 1.0f);
+	Point const insidePoint2(2.0f, 2.0f);
+	Point const outsidePoint1(1.0f, 6.0f);
+	Point const outsidePoint2(-1.5f, 2.0f);
+	Point const onVertexPoint(5.0f, 0.0f);
+	Point const onEdgePoint(0.0f, 2.5f);
+
+	testPoint(a, b, c, insidePoint1);
+	testPoint(a, b, c, insidePoint2);
+	testPoint(a, b, c, outsidePoint1);
+	testPoint(a, b, c, outsidePoint2);
+	testPoint(a, b, c, onVertexPoint);
+	testPoint(a, b, c, onEdgePoint);
+
+	return 0;
+}
