cd88d3f097fea5f1c0d729cf8b08c2e5c67c84df
Mariia Osokina <maria.v.osokina@gmail.com>
2025-09-03 02:14:28 +0100

    ex02 is in progress

--- cpp02/ex03/src/Point.cpp
+++ cpp02/ex03/src/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 10:02:32 by mosokina          #+#    #+#             */
-/*   Updated: 2025/08/26 18:58:05 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/09/03 00:20:50 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,3 @@ Fixed	Point::getY(void) const
 {
 	return (this->_y);
 };
-
--- cpp02/ex03/src/bsp.cpp
+++ cpp02/ex03/src/bsp.cpp
@@ -6,14 +6,13 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 09:38:41 by mosokina          #+#    #+#             */
-/*   Updated: 2025/08/26 19:20:44 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/09/03 00:23:47 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Point.hpp"
 
-/*
-Binary Space Partitioning:
+/*Binary Space Partitioning:
 
 1. Define a Plane for Each Edge
 
@@ -22,20 +21,19 @@ For edge AB and point P, the cross product is:
 (Bx - Ax)(Py - Ay()) - (By() - Ay())(Px - Ax)
 
 3. Check the Sign of cross products: 
-Positive: P is on one side ((e.g., all positive or all negative, depending on the vertex ordering))
-Negative: P is on the other side
-Zero: P is on the line
-
-*/
+P is on one side ((e.g., all positive or all negative, 
+depending on the vertex ordering))*/
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed d1 = (b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX()); //  cross-product for vecAB and vecAP
-	Fixed d2 = (c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX()); //  cross-product  for vecBC and vecBP
-	Fixed d3 = (a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX()); //  cross-product  for vecCA and vecCP
-
-	// The "> 0" or "< 0" depends on the triangle's winding order.
-	bool has_neg = (d1 <= 0) || (d2 <= 0) || (d3 <= 0); //  checks if any of the cross-products are negative.
-	bool has_pos = (d1 >= 0) || (d2 >= 0) || (d3 >= 0); // checks if any are positive.
-	return !(has_neg && has_pos); 
+	Fixed d1 = (b.getX() - a.getX()) * (point.getY() - a.getY())
+		- (b.getY() - a.getY()) * (point.getX() - a.getX()); // for vecAB and vecAP
+	Fixed d2 = (c.getX() - b.getX()) * (point.getY() - b.getY())
+		- (c.getY() - b.getY()) * (point.getX() - b.getX()); // for vecBC and vecBP
+	Fixed d3 = (a.getX() - c.getX()) * (point.getY() - c.getY())
+		- (a.getY() - c.getY()) * (point.getX() - c.getX()); // for vecCA and vecCP
+	if (d1 == 0 || d2 == 0 || d3 == 0)// Point is on an edge
+		return (false);
+	bool sameSign = (d1 > 0 && d2 > 0 && d3 > 0) || (d1 < 0 && d2 < 0 && d3 < 0);
+	return (sameSign);
 }
