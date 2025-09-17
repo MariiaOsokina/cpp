97180d220e416a77898a7d65afc7f03aa2851c79
Mariia Osokina <maria.v.osokina.com>
2025-09-17 23:28:31 +0100

    ex01 is in progress

---
+++ cpp05/ex01/include/Bureaucrat.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 15:11:52 by mosokina          #+#    #+#             */
+/*   Updated: 2025/09/17 22:39:51 by mosokina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <string>
+#include <iostream>
+
+
+/*NESTED CLASS: is a class defined inside another class, 
+Exception classes should be nested. */
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator = (const Bureaucrat &other);
+		~Bureaucrat();
+		
+		const std::string &getName() const;
+		int getGrade() const;
+		void increaseGrade();
+		void decreaseGrade();
+
+
+		// Nested exception classes
+		class GradeTooHighException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+
+	private:
+		const std::string _name;
+		int	_grade;
+};
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat const& other);
+
+#endif
