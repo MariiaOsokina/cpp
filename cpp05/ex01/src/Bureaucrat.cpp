fe515339c6f32ba57b67e55e6047d822fb5b6266
Mariia Osokina <maria.v.osokina@gmail.com>
2025-09-17 23:28:31 +0100

    ex01 is in progress

---
+++ cpp05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 15:11:49 by mosokina          #+#    #+#             */
+/*   Updated: 2025/09/17 23:28:10 by mosokina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Bureaucrat.h"
+
+/*Since exceptions inherit from std::exception, you can override the what() function*/
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high! Must be between 1 and 150.";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low! Must be between 1 and 150.";
+}
+
+
+
+Bureaucrat::Bureaucrat(): _name("default_name"), _grade(150)
+{
+	std::cout << "Bureaucrat default constructor called." << std::endl;
+}
+
+/*f an exception is thrown in a constructor, the object is not fully created.
+The constructor is immediately exited, and the object's memory is deallocated.
+The program immediately jumps out of the constructor and looks for a catch block.*/
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade): _name(name), _grade(grade)
+{
+	std::cout << "Bureaucrat constructor called." << std::endl;
+	if (_grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other): _name(other._name)
+{
+	std::cout << "Bureaucrat copy constructor called." << std::endl;
+	this->_grade = other._grade;
+}
+
+//_name is const can't be chaged by the operator. find solution??
+
+Bureaucrat &Bureaucrat::operator = (const Bureaucrat &other)
+{
+	std::cout << "Bureaucrat copy assignment operator called." << std::endl;
+	if (this != &other)
+	{
+		this->_grade = other._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called."<< std::endl;
+}
+
+const std::string &Bureaucrat::getName() const
+{
+	return this->_name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return this->_grade;
+}
+
+void Bureaucrat::increaseGrade()
+{
+	std::cout << "increaseGrade called."<< std::endl;
+	if (_grade - 1 < 1)
+		throw GradeTooHighException();
+	_grade --;
+}
+
+void Bureaucrat::decreaseGrade()
+{
+	std::cout << "decreaseGrade called."<< std::endl;
+	if (_grade + 1 > 150)
+		throw GradeTooLowException();
+	_grade ++;
+}
+
+
+std::ostream& operator<<(std::ostream& os, Bureaucrat const& other)
+{
+	os << other.getName() << ", bureaucrat grade " << other.getGrade() << ".";
+	return (os);
+}
+
+
---
+++ cpp05/ex01/src/main.cpp
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 15:03:10 by mosokina          #+#    #+#             */
+/*   Updated: 2025/09/17 23:18:27 by mosokina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Bureaucrat.h"
+
+
+/*EXCEPTIONS: flow
+1. Code inside try { ... } executes.
+
+2. If no exception is thrown → catch is skipped.
+
+3. If an exception is thrown (e.g., throw ...;) → execution jumps immediately to the matching catch.
+
+4. If no matching catch is found → the program calls std::terminate() (usually crashes).
+*/
+
+void	test1(void)
+{
+	try
+	{
+		std::cout << "\033[33m" << "Test1: successful creation and printing" << "\033[0m" << std::endl;
+		Bureaucrat b("Anna", 75);
+		std::cout << b << std::endl; // Test operator<< overload
+		b.increaseGrade();
+		std::cout << "After increasing grade: " << b << std::endl;
+		b.decreaseGrade();
+		std::cout << "After decreasing grade: " << b << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Caught an unexpected exception: " << e.what() << '\n';
+	}
+}
+
+void	test2(void)
+{
+	std::cout << "\033[33m" << "Test2:  instantiate a Bureaucrat with an invalid grade (low and high) "
+					<< "\033[0m" << std::endl;
+	try
+	{
+		Bureaucrat b1("John", 200);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
+	
+	try
+	{
+		Bureaucrat b2("Bill", -5);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
+}
+
+void	test3(void)
+{
+	std::cout << "\033[33m" << "Test3: grade goes out of range after its increasement / decreasement"
+					<< "\033[0m" << std::endl;
+	try
+	{
+		Bureaucrat b1("Bob", 150);
+		std::cout << b1 << std::endl;
+		b1.decreaseGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat b2("Sam", 1);
+		std::cout << b2 << std::endl;
+		b2.increaseGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << '\n';
+	}
+}
+
+void test4(void)
+{
+	std::cout << "\033[33m" << "Test4: Copy constructor" << "\033[0m" << std::endl;
+	try
+	{
+		Bureaucrat b1("Original", 50);
+		std::cout << b1 << std::endl;
+
+		Bureaucrat b2(b1);
+		std::cout << "Copied bureaucrat (b2): " << b2 << std::endl;
+		b1.increaseGrade();
+		std::cout << "Original after grade increase: " << b1 << std::endl;
+		std::cout << "Copied bureaucrat (b2) remains unchanged: " << b2 << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Caught an unexpected exception: " << e.what() << '\n';
+	}
+}
+
+void test5(void)
+{
+	std::cout << "\033[33m" << "Test5: Copy assignment operator" << "\033[0m" << std::endl;
+	try
+	{
+		Bureaucrat b1("Original", 50);
+		std::cout << b1 << std::endl;
+
+		Bureaucrat b3("Another One", 100);
+		std::cout << "Before assignment, b3 is: " << b3 << std::endl;
+		b3 = b1;
+		std::cout << "After assignment, b3 is: " << b3 << std::endl;
+		std::cout << "Note: b3's name remains constant, only the grade is updated." << std::endl;
+		std::cout << "Original bureaucrat (b1) remains unchanged: " << b1 << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Caught an unexpected exception: " << e.what() << '\n';
+	}
+}
+
+int main (void)
+{
+	test1();
+	test2();
+	test3();
+	test4();
+	test5();
+
+	return 0;
+}
