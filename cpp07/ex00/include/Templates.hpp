108192614b2bc5500e4270b26b239bc539389a21
Mariia Osokina <mosokina@c1r5s1.42london.com>
2025-10-01 16:58:52 +0100

    cpp07 is in progress

--- cpp07/ex00/include/Templates.hpp
+++ cpp07/ex00/include/Templates.hpp
@@ -6,34 +6,30 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:33:12 by mosokina          #+#    #+#             */
-/*   Updated: 2025/09/30 22:07:59 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:39:25 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEMPLATES_HPP
 #define TEMPLATES_HPP
 
-
 /*Type Template Parameters:
 The keyword typename indicates that T is a placeholder for a type
 (like int, double, or a custom class).*/
 
 template <typename T>
-
 T const &max(T const &x, T const &y)
 {
 	return (x > y ? x : y);
 }
 
 template <typename T>
-
 T const &min(T const &x, T const &y)
 {
 	return (x < y ? x : y);
 }
 
 template <typename T>
-
 void swap(T &x, T &y)
 {
 	T tempValue;
--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:41:03 by mosokina          #+#    #+#             */
-/*   Updated: 2025/09/30 23:24:59 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:57:48 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,35 @@ void	printDay(const std::string& day)
 
 void    countCicleArea(const int& radius)
 {
-    const double PI = 3.14159265358979323846; 
-    double area = PI * (double)radius * (double)radius;
-    std::cout << "The area of a circle with radius " << radius << " is " << area << std::endl;
+	const double PI = 3.14159265358979323846; 
+	double area = PI * (double)radius * (double)radius;
+	std::cout << "The area of a circle with radius " << radius << " is " << area << std::endl;
+}
+
+void    increment(int& num)
+{
+	num++;
 }
 
 int main(void)
 {
-    // String Array Test
-    const size_t lenghtDays = 7;
-    std::string days[lenghtDays] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-    iter(days, lenghtDays, printDay);
-    std::cout << std::endl;
+	// String Array Test
+	const size_t lenghtDays = 7;
+	std::string days[lenghtDays] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	iter(days, lenghtDays, printDay);
+	std::cout << std::endl;
 
-    // Integer Array Test (Calling countCicleArea)
-    const size_t lenghtNum = 7;
-    int radius[lenghtNum] = {1, 42, 7, 100, 0, 120, 10}; 
-    iter(radius, lenghtNum, countCicleArea);
+	// Integer Array Test (Calling countCicleArea)
+	const size_t lenghRadiuses = 7;
+	const int radius[lenghRadiuses] = {1, 42, 7, 100, 0, 120, 10}; 
+	iter(radius, lenghRadiuses, countCicleArea);
 
-    return 0;
+	
+	// Non Const Integer Array  (Calling Increasement)
+	
+	const size_t lenghtNum = 3;
+	int numbers[lenghtNum] = {1, -42, 42};
+	iter(numbers, lenghtNum, increment);
+	
+	return 0;
 }
