a45142b11d69967fd59d9eabb7a015d755d9a6d6
Mariia Osokina <mosokina@c1r5s1.42london.com>
2025-10-01 16:58:52 +0100

    cpp07 is in progress

--- cpp07/ex01/include/iter.hpp
+++ cpp07/ex01/include/iter.hpp
@@ -6,17 +6,16 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:17:04 by mosokina          #+#    #+#             */
-/*   Updated: 2025/09/30 23:14:06 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:51:51 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
 #include <iostream>
+#include <cstddef> // Include for size_t 
 
-template <typename T, typename F>
 
 /* The template automatically supports both const and non-const arrays because the array parameter's type, 
 defined as a generic pointer T*, naturally carries the constness of the elements*/
@@ -25,9 +24,11 @@ defined as a generic pointer T*, naturally carries the constness of the elements
 the compiler deduces T as int. The elements are mutable.
 When you pass an array of const elements (e.g., const int arr[]), 
 the compiler deduces T as const int. The elements are read-only.*/
-
+template <typename T, typename F>
 void iter(T *array, const size_t length, F function)
 {
+	if (!array)
+		return ;
 	for (size_t i = 0; i < length; ++i)
 	{
 		function(array[i]);
