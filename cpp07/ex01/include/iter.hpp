150faeb0d7acca74a3d02b6102144d64cdf4c8bd
Mariia Osokina <maria.v.osokina@gmail.com>
2025-10-02 21:35:57 +0100

    cpp07 is done

--- cpp07/ex01/include/iter.hpp
+++ cpp07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:17:04 by mosokina          #+#    #+#             */
-/*   Updated: 2025/10/01 16:51:51 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/10/02 13:54:09 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 #define ITER_HPP
 
 #include <iostream>
-#include <cstddef> // Include for size_t 
-
 
 /* The template automatically supports both const and non-const arrays because the array parameter's type, 
 defined as a generic pointer T*, naturally carries the constness of the elements*/
@@ -25,11 +23,11 @@ the compiler deduces T as int. The elements are mutable.
 When you pass an array of const elements (e.g., const int arr[]), 
 the compiler deduces T as const int. The elements are read-only.*/
 template <typename T, typename F>
-void iter(T *array, const size_t length, F function)
+void iter(T *array, const std::size_t length, F function)
 {
 	if (!array)
 		return ;
-	for (size_t i = 0; i < length; ++i)
+	for (std::size_t i = 0; i < length; ++i)
 	{
 		function(array[i]);
 	}
--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:41:03 by mosokina          #+#    #+#             */
-/*   Updated: 2025/10/01 16:57:48 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/10/02 13:36:07 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 
 #include <iostream>
 #include <string> 
-#include <cstddef>
-
 
 void	printDay(const std::string& day)
 {
 	std::cout << "Today is " << day << std::endl;
 }
 
+void	printInt(const int& number)
+{
+	std::cout << number << std::endl;
+}
+
 void    countCicleArea(const int& radius)
 {
 	const double PI = 3.14159265358979323846; 
 	double area = PI * (double)radius * (double)radius;
-	std::cout << "The area of a circle with radius " << radius << " is " << area << std::endl;
+	std::cout << "Radius: " << radius << ", Area: " << area << std::endl;
 }
 
 void    increment(int& num)
@@ -36,23 +39,30 @@ void    increment(int& num)
 
 int main(void)
 {
-	// String Array Test
-	const size_t lenghtDays = 7;
+	std::cout << "\n\033[33m" << "TEST1: String Array" << "\033[0m" << std::endl;
+	const std::size_t lenghtDays = 7;
 	std::string days[lenghtDays] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 	iter(days, lenghtDays, printDay);
 	std::cout << std::endl;
 
-	// Integer Array Test (Calling countCicleArea)
-	const size_t lenghRadiuses = 7;
+	std::cout << "\n\033[33m" << "TEST2: Integer Array (Calling countCicleArea)" << "\033[0m" << std::endl;
+	const std::size_t lenghRadiuses = 7;
 	const int radius[lenghRadiuses] = {1, 42, 7, 100, 0, 120, 10}; 
 	iter(radius, lenghRadiuses, countCicleArea);
+	std::cout << std::endl;
 
-	
-	// Non Const Integer Array  (Calling Increasement)
-	
-	const size_t lenghtNum = 3;
+	std::cout << "\n\033[33m" << "TEST3: Non Const Integer Array  (Calling Increasement)" << "\033[0m" << std::endl;
+	const std::size_t lenghtNum = 3;
 	int numbers[lenghtNum] = {1, -42, 42};
+	std::cout << "Before increasing:" << std::endl;
+	iter(numbers, lenghtNum, printInt);
 	iter(numbers, lenghtNum, increment);
-	
+	std::cout << "After increasing:" << std::endl;
+	iter(numbers, lenghtNum, printInt);
+
+	// The following would cause a compiler error, proving const-safety:
+	// const int numbersConst[lenghtNum] = {1, -42, 42};
+	// iter(numbersConst, lenghtNum, increment);
+
 	return 0;
 }
