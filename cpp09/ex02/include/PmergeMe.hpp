91abae0987e5c72869dfb3c3b4428d6ab8064c7c
Mariia Osokina <mosokina@c1r3s1.42london.com>
2025-11-05 16:59:47 +0000

    ex02 PmergeMe for vectors is done

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 13:54:50 by mosokina          #+#    #+#             */
-/*   Updated: 2025/11/04 11:36:15 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/11/05 16:41:08 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 #include <iostream>
 #include <cstddef> //for size_t 
 #include <cmath>
+
+
 class PmergeMe
 {
 	public:
@@ -28,35 +30,25 @@ class PmergeMe
 		~PmergeMe();
 
 		typedef std::vector<int>::iterator vecIt;
-		// typedef std::vector<vecIt>::iterator vecItIt;
 		
 		void mergeInsertSort(std::vector<int> &vec, vecIt &levelLastElemIt, size_t level);
-		size_t getNmbComp() const;
 
 		void printVector(const std::vector<int>& vec);
-		void printItVector(const std::vector<vecIt>& vec);
+		// void printItVector(const std::vector<vecIt>& vec);
+
+		static bool compIetrators(vecIt lv, vecIt rv);
+		static size_t nmbComp;
+
 
 	private:
-		// vecIt _next(vecIt &it, int dist);
-		std::vector<vecIt>::iterator _next(std::vector<vecIt>::iterator it, int dist);
 		void _sortPairs(std::vector<int>& vec, size_t elementsInLevel, size_t nmbsInBlock);
-		void _storePend(std::vector<int>& vec, vecIt lBlockStart, vecIt rBlockStart);
-		// std::vector<int> _createMain(std::vector<int>& vec, vecIt &levelLastElemIt, size_t nmbsInBlock);
-		std::vector<vecIt> _createMain(std::vector<int>& vec, vecIt &levelLastElemIt, size_t nmbsInBlock);
-		// std::vector<int> _createPend(std::vector<int>& vec, vecIt &levelLastElemIt, size_t nmbsInBlock);
-	
+		std::vector<vecIt> _createMain(std::vector<int>& vec, vecIt &levelLastElemIt, size_t nmbsInBlock);	
 		std::vector<vecIt> _createPend(std::vector<int>& vec, vecIt &levelLastElemIt, size_t nmbsInBlock);
 		void _insertPendToMain(std::vector<vecIt>& main, std::vector<vecIt>& pend);
+		long _jacobsthalNumber(long n);
 		void _jackNumInvertion(std::vector<vecIt>& main, std::vector<vecIt>& pend);
 		void _orderedInvertion(std::vector<vecIt>& main, std::vector<vecIt>& pend);
-
 		void _copyMainToVec(std::vector<int>& vec, std::vector<vecIt>& main, size_t nmbsInBlock);
-		long _jacobsthalNumber(long n);
-
-		static bool _comp(vecIt lv, vecIt rv); //??static
-
-		size_t _nmbComp;
-		std::vector<int> _pendulums;
 };
 
 #endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -6,72 +6,54 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:26:28 by mosokina          #+#    #+#             */
-/*   Updated: 2025/11/04 13:35:32 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/11/05 16:58:40 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PmergeMe.hpp"
 
+#include <cmath>
 
-// 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
+size_t PmergeMe::nmbComp = 0;
 
-PmergeMe::PmergeMe()
-{
-    std::cout << "PmergeMe: Default constructor called." << std::endl;
-}
+PmergeMe::PmergeMe(){}
 
 PmergeMe::PmergeMe(const PmergeMe &other)
 {
-    (void)other; // Mark 'other' as unused to suppress compiler warnings
-    std::cout << "PmergeMe: Copy constructor called." << std::endl;
-    // Example for copying a member 'sorted_data':
-    // this->sorted_data = other.sorted_data;
+	(void)other;
 }
 
 PmergeMe& PmergeMe::operator=(const PmergeMe &other)
 {
-    std::cout << "PmergeMe: Assignment operator called." << std::endl;
-    if (this != &other)
-    {
-        // 1. Clean up existing resources if necessary (not needed here)
-        
-        // 2. Perform deep copy of members
-        // Example for assigning a member 'sorted_data':
-        // this->sorted_data = other.sorted_data;
-        (void)other; // Mark 'other' as unused
-    }
-    return *this;
+	if (this != &other)
+		(void)other;
+	return *this;
 }
 
-PmergeMe::~PmergeMe()
-{
-    std::cout << "PmergeMe: Destructor called." << std::endl;
-}
+PmergeMe::~PmergeMe(){}
 
 void PmergeMe::printVector(const std::vector<int>& vec)
 {
-	std::cout << "[";
+	// std::cout << "[";
 	for (size_t i = 0; i < vec.size(); ++i) {
 		std::cout << vec[i] << " ";
 	}
-	std::cout << "]" << std::endl;
+	// std::cout << "]";
+	std::cout << std::endl;
+
 }
 
-void PmergeMe::printItVector(const std::vector<vecIt>& vec)
-{
-	std::cout << "[";
-	for (size_t i = 0; i < vec.size(); ++i) {
-		std::cout << *(vec[i]) << " ";
-	}
-	std::cout << "]" << std::endl;
-}
+// void PmergeMe::printItVector(const std::vector<vecIt>& vec)
+// {
+// 	std::cout << "[";
+// 	for (size_t i = 0; i < vec.size(); ++i) {
+// 		std::cout << *(vec[i]) << " ";
+// 	}
+// 	std::cout << "]" << std::endl;
+// }
 
-size_t PmergeMe::getNmbComp() const
-{
-	return (this->_nmbComp);
-}
 
-void PmergeMe::_sortPairs(std::vector<int>& vec, size_t pairsInLevel, size_t nmbsInBlock) //?
+void PmergeMe::_sortPairs(std::vector<int>& vec, size_t pairsInLevel, size_t nmbsInBlock)
 {
 	vecIt lBlockStart = vec.begin();
 	vecIt rBlockStart, lBlockEnd, rBlockEnd;
@@ -84,7 +66,7 @@ void PmergeMe::_sortPairs(std::vector<int>& vec, size_t pairsInLevel, size_t nmb
 
 		if (*lBlockEnd > *rBlockEnd) // Comparison and Swap
 		{
-			_nmbComp++;
+			PmergeMe::nmbComp++;
 			vecIt leftMover = lBlockStart;
 			vecIt rightMover = rBlockStart;
 			for (size_t j = 0; j < nmbsInBlock; j++)
@@ -106,6 +88,7 @@ std::vector<std::vector<int>::iterator> PmergeMe::_createPend(std::vector<int>& 
 	vecIt startIt  = vec.begin();
 	vecIt lBlockStart = startIt + (nmbsInBlock * 2);
 	vecIt lBlockLast = lBlockStart + (nmbsInBlock - 1);
+
 	while (lBlockStart < levelLastElemIt) // while (rBlockLast < levelLastElemIt)??
 	{
 		pend.insert(pend.end(), lBlockLast);
@@ -123,16 +106,18 @@ std::vector<std::vector<int>::iterator> PmergeMe::_createMain(std::vector<int>& 
 	
 	vecIt startIt  = vec.begin();
 	main.insert(main.end(), startIt + (nmbsInBlock - 1));
-    main.insert(main.end(), startIt + (nmbsInBlock * 2 - 1));
+	main.insert(main.end(), startIt + (nmbsInBlock * 2 - 1));
 	
 	vecIt rBlockStart = startIt + (nmbsInBlock * 3);
 	vecIt rBlockLast = rBlockStart + (nmbsInBlock - 1);
+
 	while (rBlockStart < levelLastElemIt) // while (rBlockLast < levelLastElemIt)
 	{
 		main.insert(main.end(), rBlockLast);
 		std::advance(rBlockStart, nmbsInBlock * 2); // move to next element
 		std::advance(rBlockLast, nmbsInBlock * 2); // move to next element
 	}
+
 	return main;
 }
 
@@ -140,8 +125,6 @@ std::vector<std::vector<int>::iterator> PmergeMe::_createMain(std::vector<int>& 
 to optimize the number of comparisons.
 
 TO ADD!!!!!!!!! 3, 5, 11, ..
-
-
 
 */
 
@@ -164,22 +147,16 @@ void PmergeMe::_jackNumInvertion(std::vector<vecIt>& main, std::vector<vecIt>& p
 		int currJN = _jacobsthalNumber(k);
 
 		int jackDiff =  currJN - prevJN;
-		std::cout << "TEST1: prevJacNum " << prevJN <<std::endl;
-		std::cout << "TEST1: currJN " << currJN <<std::endl;
-		std::cout << "TEST1: jackDiff " << jackDiff <<std::endl;
 		int offset = 0;
 		if (jackDiff > static_cast<int>(pend.size()))
 			break ;
 		
 		std::vector<vecIt>::iterator pendIt = pend.begin() + (jackDiff - 1);
 		std::vector<vecIt>::iterator boundIt = main.begin() + (currJN + insertedCount);
-		// std::cout << "TEST4 boundIt it: " << boundIt << std::endl;
 
 		for (size_t i = jackDiff; i > 0; --i)
 		{
-			std::vector<vecIt>::iterator idxToInsert = std::upper_bound(main.begin(), boundIt, *pendIt, _comp);
-			std::cout << "TEST3 pend it: " << "\n\033[33m" << *(*pendIt) << "\n\033[0m" << std::endl;
-			std::cout << "TEST5!!!!!!! idxToInsert it: " << "\n\033[33m" << *(*idxToInsert) << "\n\033[0m" << std::endl;
+			std::vector<vecIt>::iterator idxToInsert = std::upper_bound(main.begin(), boundIt, *pendIt, compIetrators);
 
 			std::vector<vecIt>::iterator inserted = main.insert(idxToInsert, *pendIt);
 			pendIt = pend.erase(pendIt);
@@ -188,57 +165,27 @@ void PmergeMe::_jackNumInvertion(std::vector<vecIt>& main, std::vector<vecIt>& p
 					offset ++;
 			boundIt = main.begin() + (currJN + insertedCount - offset);
 			
-			
-			std::cout << "TEST2 pend after: " << std::endl;
-			this->printItVector(pend);
-			std::cout << "TEST2 main after: " << std::endl;
-			this->printItVector(main);
 		}
 		prevJN = currJN;
 		insertedCount += jackDiff;
 	}
 }
 
-
-// /* Insert the remaining elements in the reversed order. Here we also want to
-//        perform as less comparisons as possible, so we calculate the starting bound
-//        to insert pend number to be the pair of the first pend number. If the first
-//        pend number is b8, the bound is a8, if the pend number is b7, the bound is a7 etc.
-//        With the way I do it the index of bound is
-//        size_of_main - size_of_pend + index_of_current_pend. */
-//     for (ssize_t i = pend.size() - 1; i >= 0; i--)
-//     {
-//         typename std::vector<Iterator>::iterator curr_pend = next(pend.begin(), i);
-//         typename std::vector<Iterator>::iterator curr_bound =
-//             next(main.begin(), main.size() - pend.size() + i + is_odd);
-//         typename std::vector<Iterator>::iterator idx =
-//             std::upper_bound(main.begin(), curr_bound, *curr_pend, _comp<Iterator>);
-//         main.insert(idx, *curr_pend);
-//     }
-
 void PmergeMe::_orderedInvertion(std::vector<vecIt>& main, std::vector<vecIt>& pend)
 {
+	if (pend.empty())
+		return ;	
 	bool isOdd = false;
 	if (pend.size() + main.size() % 2 != 0)
 		isOdd = true;
-	for (size_t i = pend.size() - 1; i >= 0; i--)
+	for (ssize_t i = pend.size() - 1; i >= 0; i--)
 	{
-		std::vector<vecIt>::iterator pendIt = pend.begin() + (i - 1);
-		std::vector<vecIt>::iterator boundIt = main.begin() + (main.size() - 1  + i + isOdd);
-		std::vector<vecIt>::iterator idxToInsert = std::upper_bound(main.begin(), boundIt, *pendIt, _comp);
-		
-		std::cout << "TEST3 pend it: " << "\n\033[33m" << *(*pendIt) << "\n\033[0m" << std::endl;
-		std::cout << "TEST5!!!!!!! idxToInsert it: " << "\n\033[33m" << *(*idxToInsert) << "\n\033[0m" << std::endl;
-		
-		std::vector<vecIt>::iterator inserted = main.insert(idxToInsert, *pendIt);
-		(void)inserted;
-		pendIt = pend.erase(pendIt);
-		
-		
-		std::cout << "TEST6 pend after: " << std::endl;
-		this->printItVector(pend);
-		std::cout << "TEST6 main after: " << std::endl;
-		this->printItVector(main);
+		std::vector<vecIt>::iterator pendIt = pend.begin() + i;
+		std::vector<vecIt>::iterator boundIt =
+			main.begin() + (main.size() - pend.size() + i + isOdd);
+		std::vector<vecIt>::iterator idxToInsert =
+			std::upper_bound(main.begin(), boundIt, *pendIt, compIetrators);
+		main.insert(idxToInsert, *pendIt);
 	}
 }
 
@@ -247,8 +194,6 @@ void PmergeMe::_insertPendToMain(std::vector<vecIt>& main, std::vector<vecIt>& p
 	if (pend.empty())
 		return ;
 	_jackNumInvertion(main, pend);
-	std::cout << "TESTTTTTTTTTTTTTTTTTTTTTTTT" << std::endl;
-
 	_orderedInvertion(main, pend);
 }
 
@@ -256,16 +201,19 @@ void PmergeMe::_copyMainToVec(std::vector<int>& vec, std::vector<vecIt>& main, s
 {
 	std::vector<int> tmpVec;
 	tmpVec.reserve(vec.size());
+	
 	for (size_t i = 0; i < static_cast<size_t>(main.size()); i ++)
 	{
 		std::vector<vecIt>::iterator itFromMain = main.begin() + i;
-		vecIt itInBlock = *itFromMain - (nmbsInBlock + 1);
+
+		vecIt itInBlock = *itFromMain - (nmbsInBlock - 1);
 		for (size_t j = 0; j < nmbsInBlock; j++) //copy numbers in Block
 		{
 			tmpVec.insert(tmpVec.end(), *itInBlock);
 			itInBlock ++;
 		}
 	}
+
 	vecIt tmpIt  = tmpVec.begin();
 	vecIt vecIt  = vec.begin();
 	while(tmpIt != tmpVec.end())
@@ -273,77 +221,42 @@ void PmergeMe::_copyMainToVec(std::vector<int>& vec, std::vector<vecIt>& main, s
 		*vecIt = *tmpIt;
 		tmpIt ++;
 		vecIt ++;
-	}
+	}		
 }
 
 void PmergeMe::mergeInsertSort(std::vector<int> &vec, vecIt &levelLastElemIt, size_t level)
 {
-	
-	std::cout << "TEST level : " << level << std::endl;
 
 	vecIt startIt  = vec.begin();
 	
-	size_t totalNmbsInLevel = levelLastElemIt - startIt + 1; //?? +1
-	// std::cout << "TEST totalNmbsInLevel : " << totalNmbsInLevel << std::endl;
-
+	size_t totalNmbsInLevel = levelLastElemIt - startIt + 1;
 	if (totalNmbsInLevel <= 1) //empty or one nbr
 		return ;
 
 	size_t nmbsInPair = 1 << level; //2 , 4, 8, 16, 32, ... 
-
 	if (nmbsInPair > totalNmbsInLevel)
 		return ;
 
 	size_t nmbsInBlock = nmbsInPair / 2;
 	size_t pairsInLevel = totalNmbsInLevel / nmbsInPair;
-
-	// std::cout << "TEST nmbsInPair : " << nmbsInPair << std::endl;
-	// std::cout << "TEST nmbsInBlock : " << nmbsInBlock << std::endl;
-	// std::cout << "TEST pairsInLevel : " << pairsInLevel << std::endl;
-
 	_sortPairs(vec, pairsInLevel, nmbsInBlock);
-	// std::cout << "TEST after  _sortPairs: " << std::endl;
-	this->printVector(vec);
-
 	vecIt newLevelEndIt = startIt + (pairsInLevel * nmbsInPair) - 1;
-	// std::cout << "TEST newLevelEndIt : " << *newLevelEndIt << std::endl;
 
 	mergeInsertSort(vec, newLevelEndIt, level + 1); ////sort recursion
-
-	std::vector<std::vector<int>::iterator> pend = _createPend(vec, levelLastElemIt, nmbsInBlock);
-	std::cout << "\n\033[33m" << "TEST pend level : " << level << "\n\033[0m" << std::endl;
-	std::cout << "TEST vec: " << std::endl;
-	this->printVector(vec);
-	std::cout << "TEST pend before: " << std::endl;
-	this->printItVector(pend);
 	
-	std::cout << "TEST main before: " << std::endl;
-
 	std::vector<std::vector<int>::iterator> main = _createMain(vec, levelLastElemIt, nmbsInBlock);
-	this->printItVector(main);
-
-	std::cout << "TEST main and pend after insert: " << std::endl;
-
+	std::vector<std::vector<int>::iterator> pend = _createPend(vec, levelLastElemIt, nmbsInBlock);
+	if (level == 1 && vec.size() % 2 != 0) // the last odd element
+		pend.push_back(levelLastElemIt);
+	
 	_insertPendToMain(main, pend);
+	_copyMainToVec(vec, main, nmbsInBlock);
 
-	std::cout << "TEST pend after: " << std::endl;
-	this->printItVector(pend);
-	std::cout << "TEST main after: " << std::endl;
-	this->printItVector(main);
-
-	// _copyMainToVec(vec, main, nmbsInBlock);
 }
 
-// PmergeMe::vecIt PmergeMe::_next(vecIt &it, int dist)
-// {
-// 	vecIt newIt(it);
-// 	std::advance(newIt, dist);
-// 	return newIt;
-// }
-
-
-bool PmergeMe::_comp(vecIt lv, vecIt rv)
+bool PmergeMe::compIetrators(vecIt lv, vecIt rv) //static function
 {
-	// _nmbComp++;
-	return *lv < *rv;
+	PmergeMe::nmbComp++;
+	return *lv <= *rv;
 }
+
