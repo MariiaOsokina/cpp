7bfddeee12c6428b31880cdacdb02c4edf10c56b
Mariia Osokina <maria.v.osokina@gmail.com>
2025-11-20 13:29:01 +0000

    ex00 is updated

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 12:34:54 by mosokina          #+#    #+#             */
-/*   Updated: 2025/10/17 15:23:43 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/11/20 13:18:15 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 #include <map>
 #include <climits>
 #include <stdexcept> // For std::exception, though specific type is std::ios_base::failure
+#include <sstream>
+#include <cstdlib> // For strtol
+#include <cerrno>  // For errno and ERANGE
 
 #define CSV_FILE "data.csv"
 
@@ -31,12 +34,94 @@ class Btc
 		Btc(const Btc &other);
 		Btc& operator = (const Btc &other);
 		~Btc();
-		bool parsingExchangRate(std::string &filename);
-		void calculateResult(std::string &dateToFind, float value);
-
 		typedef std::map<std::string,float>::iterator iterator;
 
+		void execute(const std::string &inputFile);
+
+		//exceptions
+		class CouldNotOpenFile : public std::exception
+		{
+			public:
+				virtual const char *what() const throw()
+				{
+					return ("Could not open file");
+				}
+		};
+
+		class InvalidInput : public std::exception
+		{
+			private:
+				std::string _message; // Store the dynamic message
+			public:
+				InvalidInput(const std::string& msg) : _message(msg) {}
+				virtual const char *what() const throw()
+				{
+					return _message.c_str();
+				}
+				
+				virtual ~InvalidInput() throw() {} //??
+		};
+
+		class InvalidDatabase : public std::exception
+		{
+			private:
+				std::string _message; // Store the dynamic message
+			public:
+				InvalidDatabase(const std::string& msg) : _message(msg) {}
+				virtual const char *what() const throw()
+				{
+					return _message.c_str();
+				}
+				
+				virtual ~InvalidDatabase() throw() {} //??
+		};
+
+		// class InvalidDateFormat : public std::exception
+		// {
+		// 	public:
+		// 		virtual const char *what() const throw()
+		// 		{
+		// 			return ("Invalid date format. Expected 'YYYY-MM-DD'");
+		// 		}
+		// };
+
+		// class InvalidColumnFormat : public std::exception
+		// {
+		// 	public:
+		// 		virtual const char *what() const throw()
+		// 		{
+		// 			return ("Invalid column format. Expected 'date,exchange_rate'");
+		// 		}
+		// };
+
+		// class InvalidDateFormat : public std::exception
+		// {
+		// 	public:
+		// 		virtual const char *what() const throw()
+		// 		{
+		// 			return ("Invalid date format. Expected 'YYYY-MM-DD'");
+		// 		}
+		// };
+
+		// class InvalidPriceFormat : public std::exception
+		// {
+		// 	public:
+		// 		virtual const char *what() const throw()
+		// 		{
+		// 			return ("Invalid price format. Expected a number between 0.0 and 1000.0");
+		// 		}
+		// };
+		
 	private:
-		// void parseRate();
+		void _parsingExchangRate(std::string &databaseCSV);
+		void _calculateResult(std::string &dateToFind, float value);	// void parseRate();
+		
+		bool _checkDate(std::string &dateToFind);
+		int _getMaxDays(int year, int month);
+		bool _isLeap(int year);
+		bool _stringToLong(const std::string string, long &result);
+		void _handleLine(std::string &line);
+
+
 		std::map<std::string,float> _btcMap;
 };
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mosokina <mosokina@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 11:14:57 by mosokina          #+#    #+#             */
-/*   Updated: 2025/10/21 15:32:06 by mosokina         ###   ########.fr       */
+/*   Updated: 2025/11/20 12:04:47 by mosokina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,99 +14,24 @@
 #include <stdlib.h>
 #include <cerrno>  // errno, ERANGE
 
-
-bool valideInputFile(int argc, char *argv[], std::ifstream &infile)
+int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		std::cerr << "\033[31m" << "Error: could not open file " << "\033[0m" << std::endl;
+		std::cerr << "\033[31m" << "Error: It should be ./btc [file] " << "\033[0m" << std::endl;
 		return false;
 	}
-	std::string filename = argv[1];
-	infile.open(filename.c_str());
-	if (!infile.is_open())
+
+	Btc btc;
+	try
 	{
-		std::cerr << "\033[31m" << "Error: could not open file " << filename << "\033[0m" << std::endl;
-		return false;
+		{btc.execute(argv[1]);}
 	}
-	return true;
-}
-
-long stringToLong(std::string string, long &result)
-{
-	char *endptr;
-
-	errno = 0;
-	result = strtol(string.c_str(), &endptr, 10);
-	if (errno == ERANGE) 
-		return false;
-	if (*endptr != '\0')
-		return false;
-	return true;
-}
-
-bool isLeap(int year) {
-	if (year % 400 == 0)
-		return true;
-	if (year % 100 == 0)
-		return false;
-	if (year % 4 == 0)
-		return true;
-	return false;
-}
-
-int getMaxDays(int year, int month) {
-	switch (month)
-		{
-		case 4: case 6: case 9: case 11:
-			return 30;
-		case 2:
-			return isLeap(year) ? 29 :28;
-		default:
-			return 31;
-	}
-}
-
-bool checkDate(std::string &dateToFind)
-{
-	if (dateToFind.length() != 10 || dateToFind[4] != '-' || dateToFind[4] != '-')
+	catch(const std::exception& e)
 	{
-		std::cerr << "\033[31m" << "Error: wrong format of the date " << dateToFind << "\033[0m" << std::endl;
-		return false;
+		std::cerr << e.what() << std::endl;
 	}
-	long year, month, date;
-	if (!stringToLong(dateToFind.substr(0,4), year)
-		|| !stringToLong(dateToFind.substr(5,2), month)
-		|| !stringToLong(dateToFind.substr(8,2), date))
-		{
-			std::cerr << "\033[31m" << "Error: wrong format of the date " << dateToFind << "\033[0m" << std::endl;
-			return false;
-		}
-	if (month > getMaxDays(year, month))
-	{
-		std::cerr << "\033[31m" << "Error: bag input => " << dateToFind << "\033[0m" << std::endl;
-		return false;
-	}
-	return true;
-}
-
-int main(int argc, char *argv[])
-{
-	std::string fileCsv = CSV_FILE;
-	std::ifstream infile;
-	if (!valideInputFile(argc, argv, infile))
-		return 1;
-	Btc btcTest;
-	if (! btcTest.parsingExchangRate(fileCsv))
-			return 1;
-	std::string line;
-	while (std::getline(infile, line))
-	{
-		std::string dateToFind = line.substr(0, 10);
-		long value;
-		if (checkDate(dateToFind) && stringToLong(line.substr(14, line.size()), value))
-			btcTest.calculateResult(dateToFind, value);
-	}	
+	
 	return 0;
 }
 
